b5ab35528265b59b25e62479099cb0b9d6c8ec25
katsuyanakata <postscape.nakata@gmail.com>
2022-06-30 09:09:54 +0900

    <update> modal

--- lp_sobolon/js/script.js
+++ lp_sobolon/js/script.js
@@ -1,5 +1,6 @@
-// スムーススクロール
 $(function () {
+
+    // スムーススクロール
     $('a[href^="#"]').click(function () {
         let speed = 300;
         let href = $(this).attr("href");
@@ -8,5 +9,27 @@ $(function () {
         $("html, body").animate({ scrollTop: position + 100 }, speed, "swing");
         return false;
     });
+
+
+    let item = $('.feature-item');
+    let modalClose = $('.modal-mask, .modal-close');
+    item.click(function(){
+        $('.modal').fadeIn();
+        const modalNumber = $(this).data('modal');
+        if(modalNumber === 1 ){
+            $('li:nth-child(1)').fadeIn();
+        } else if(modalNumber === 2){
+            $('li:nth-child(2)').fadeIn();
+        } else if(modalNumber === 3){
+            $('li:nth-child(3)').fadeIn();
+        }
+        return false;
+    });
+    modalClose.click(function(){
+        $('.modal, .modal-item').fadeOut();
+        return false;
+    });
+
 });
+
 
