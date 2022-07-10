41b1fb3a62c693a516af2d3707d5f7ff39246f91
katsuyanakata <postscape.nakata@gmail.com>
2022-07-10 19:59:00 +0900

    <add> PHP

---
+++ PHP/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>test</title>
+</head>
+<body>
+  <h1>
+  <?php
+    echo 1 + 2;
+    echo '</br>';
+    echo 4 - 1;
+    echo '</br>';
+    echo 12 * 12;
+    echo '</br>';
+    echo 12 / 3;
+    echo '</br>';
+    echo 'がんばるぞ！';
+  ?>
+    
+  </h1>
+</body>
+</html>
